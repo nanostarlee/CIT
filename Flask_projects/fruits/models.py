94ca834112e3749ffc21f17ecd15d850f737421c
nanostarlee <nanostarleelawrence@gmail.com>
2022-10-24 00:40:58 +0300

    Flask Projects

---
+++ Flask_projects/fruits/models.py
@@ -0,0 +1,68 @@
+from os import link
+from turtle import title
+from fruits import db
+from datetime import datetime
+
+
+class ExtraMixin(object):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+# fruits model
+class Fruit(db.Model, ExtraMixin):
+    __tablename__ = 'fruits'
+    name = db.Column(db.String(100), nullable=False)
+    color = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+#Hacker News model
+class HackerNews(db.Model, ExtraMixin):
+    __tablename__ = 'hacker_news'
+    title = db.Column(db.String(255), nullable=False)
+    link = db.Column(db.String(255), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'link': self.link,
+            'created_at': str(self.created_at),
+        }
+
+    @classmethod
+    def get_all_news(cls):
+        return cls.query.all()
+
+# CBS News model
+class CbsNews(db.Model, ExtraMixin):
+    __tablename__ = 'cbs_news'
+    image = db.Column(db.string(255), nullable = False)
+    title = db.Column(db.Text, nullable = False)
+    description = db.Column(db.Text, nullable = False)
+    link = db.Column(db.Text, nullable = False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'image': self.image,
+            'title': self.title,
+            'description': self.description,
+            'link': self.link,
+            'created_at': str(self.created_at),
+        }
+    @classmethod
+    def get_all_news(cls):
+        return cls.query.all()
+
---
+++ Flask_projects/fruits/views/cbs_news.py
@@ -0,0 +1,57 @@
+import json
+from flask import Blueprint, request, redirect, render_template
+from bs4 import BeautifulSoup
+import requests
+from fruits.models import CbsNews
+
+cbsviews = Blueprint('cbsviews', __name__)
+
+def get_cbs_news():
+    url = "https://www.cbsnews.com/latest/rss/main"
+    response = requests.get(url)
+
+    data = []
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    rows = soup.find_all('tr', class_ = 'athing')
+
+    for row in rows:
+            # extract title
+        image = row.find('span', class_ = 'titleline').find('a').string
+        title = row.find('span', class_ = 'titleline').find('a').text
+        description = row.find('span', class_ = 'titleline').find('a').text
+        link = row.find('span', class_ = 'titleline').find('a')['href']
+        
+        data.append({
+            'image': image,
+            'title': title,
+            'description': description,
+            'link': link,            
+
+        })
+
+    return data
+
+@cbsviews.route('/cbs_news', methods=['GET', 'POST'])
+def cbs_news():
+    if request.method == 'POST':
+        return redirect('/')
+
+# new data from cbs news
+    data = get_cbs_news()
+
+    # existing data from the database
+    cbsnews = CbsNews.get_all_news()
+
+    # loop through data
+    for news in data:
+        # check if news already exists in database
+        if news.get('title').lower() not in [cbsn.title.lower() for cbsn in cbsnews]:
+            cbsnew = CbsNews(image=news['image'],title=news['title'], link=news['link'],\
+                description=news['description'] )
+            cbsnew.save()
+        else:
+            continue
+
+    return render_template('cbs_news.html', cbsnews=cbsnews)
